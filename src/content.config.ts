5703589aa4b3a497938967146cc267ea587e0093
Anthony Savchenko <anthony.savchenko@gmail.com>
2025-11-16 08:16:45 +0300

    Страница художников с картинками и основными стилями

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,28 +1,31 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
+const arts = defineCollection({
+  loader: glob({ base: './src/content/arts', pattern: '**/*.{md,mdx}' }),
+  schema: ({ image }) =>
+    z.object({
+      order: z.number(),
+      title: z.string(),
+      year: z.number(),
+      artist: z.string(),
+      technic: z.string(),
+      size: z.array(z.object({
+        a: z.number(),
+        b: z.number()
+      })),
+      photo: image(),
+    }),
 });
 
 const artists = defineCollection({
-	loader: glob({ base: './src/content/artists', pattern: '**/*.{md,mdx}' }),
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			portrait: image().optional(),
-		}),
+  loader: glob({ base: './src/content/artists', pattern: '**/*.{md,mdx}' }),
+  schema: ({ image }) =>
+    z.object({
+      order: z.number(),
+      title: z.string(),
+      photo: image(),
+    }),
 });
 
-export const collections = { artists, blog };
+export const collections = { artists, arts };
