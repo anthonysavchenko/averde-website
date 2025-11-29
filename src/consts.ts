f7ccecc5b97a8e582f420728c07dc423cf68cd8c
Anthony Savchenko <anthony.savchenko@gmail.com>
2025-11-29 19:12:13 +0300

    Всплывающее окно с предупрждением

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,2 @@
-export const SITE_TITLE = 'АВердизм - новое течение в искусстве';
+export const SITE_TITLE = "АВердизм - новое течение в искусстве";
+export const ALERT_BUTTON_ID = "alert-button-id";
